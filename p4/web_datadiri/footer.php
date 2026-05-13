23544d409c35eed2da34c0b40c45b60ac7aed9d0
andrejonatan <jonatanandre01@gmail.com>
2026-05-13 22:26:55 +0700

    hapus folder p4

--- p4/web_datadiri/footer.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-primary text-center" role="alert">
-	AndrePages @ 2026
-</div>
-
--- p4/web_datadiri/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['AUTH']);
-header('Location: index.php?hal=home');
-exit;
